845ca19eb5e50eae082c8f612873b1880cb35f80
Andrea <collet.andrea@gmail.com>
2011-06-13 19:59:13 +0200

    Importing module folder structure

---
+++ dca/tl_dummyimages.php
@@ -0,0 +1,125 @@
+<?php if (!defined('TL_ROOT')) die('You cannot access this file directly!');
+
+/**
+ * Contao Open Source CMS
+ * Copyright (C) 2005-2011 Leo Feyer
+ *
+ * Formerly known as TYPOlight Open Source CMS.
+ *
+ * This program is free software: you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation, either
+ * version 3 of the License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this program. If not, please visit the Free
+ * Software Foundation website at <http://www.gnu.org/licenses/>.
+ *
+ * PHP version 5
+ * @copyright  Liquid Diamond 2011 
+ * @author     Liquid Diamond 
+ * @package    DummyImages 
+ * @license    GNU/LGPL 
+ * @filesource
+ */
+
+
+/**
+ * Table tl_dummyimages 
+ */
+$GLOBALS['TL_DCA']['tl_dummyimages'] = array
+(
+
+	// Config
+	'config' => array
+	(
+		'dataContainer'               => 'Table',
+		'enableVersioning'            => true
+	),
+
+	// List
+	'list' => array
+	(
+		'sorting' => array
+		(
+			'mode'                    => 1,
+			'fields'                  => array(''),
+			'flag'                    => 1
+		),
+		'label' => array
+		(
+			'fields'                  => array(''),
+			'format'                  => '%s'
+		),
+		'global_operations' => array
+		(
+			'all' => array
+			(
+				'label'               => &$GLOBALS['TL_LANG']['MSC']['all'],
+				'href'                => 'act=select',
+				'class'               => 'header_edit_all',
+				'attributes'          => 'onclick="Backend.getScrollOffset();" accesskey="e"'
+			)
+		),
+		'operations' => array
+		(
+			'edit' => array
+			(
+				'label'               => &$GLOBALS['TL_LANG']['tl_dummyimages']['edit'],
+				'href'                => 'act=edit',
+				'icon'                => 'edit.gif'
+			),
+			'copy' => array
+			(
+				'label'               => &$GLOBALS['TL_LANG']['tl_dummyimages']['copy'],
+				'href'                => 'act=copy',
+				'icon'                => 'copy.gif'
+			),
+			'delete' => array
+			(
+				'label'               => &$GLOBALS['TL_LANG']['tl_dummyimages']['delete'],
+				'href'                => 'act=delete',
+				'icon'                => 'delete.gif',
+				'attributes'          => 'onclick="if (!confirm(\'' . $GLOBALS['TL_LANG']['MSC']['deleteConfirm'] . '\')) return false; Backend.getScrollOffset();"'
+			),
+			'show' => array
+			(
+				'label'               => &$GLOBALS['TL_LANG']['tl_dummyimages']['show'],
+				'href'                => 'act=show',
+				'icon'                => 'show.gif'
+			)
+		)
+	),
+
+	// Palettes
+	'palettes' => array
+	(
+		'__selector__'                => array(''),
+		'default'                     => ''
+	),
+
+	// Subpalettes
+	'subpalettes' => array
+	(
+		''                            => ''
+	),
+
+	// Fields
+	'fields' => array
+	(
+		'' => array
+		(
+			'label'                   => &$GLOBALS['TL_LANG']['tl_dummyimages'][''],
+			'exclude'                 => true,
+			'inputType'               => 'text',
+			'eval'                    => array('mandatory'=>true, 'maxlength'=>255)
+		)
+	)
+);
+
+?>
---
+++ languages/en/tl_dummyimages.php
@@ -0,0 +1,53 @@
+<?php if (!defined('TL_ROOT')) die('You cannot access this file directly!');
+
+/**
+ * Contao Open Source CMS
+ * Copyright (C) 2005-2011 Leo Feyer
+ *
+ * Formerly known as TYPOlight Open Source CMS.
+ *
+ * This program is free software: you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation, either
+ * version 3 of the License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this program. If not, please visit the Free
+ * Software Foundation website at <http://www.gnu.org/licenses/>.
+ *
+ * PHP version 5
+ * @copyright  Liquid Diamond 2011 
+ * @author     Liquid Diamond 
+ * @package    DummyImages 
+ * @license    GNU/LGPL 
+ * @filesource
+ */
+
+
+/**
+ * Fields
+ */
+$GLOBALS['TL_LANG']['tl_dummyimages'][''] = array('', '');
+
+
+/**
+ * Reference
+ */
+$GLOBALS['TL_LANG']['tl_dummyimages'][''] = '';
+
+
+/**
+ * Buttons
+ */
+$GLOBALS['TL_LANG']['tl_dummyimages']['new']    = array('', '');
+$GLOBALS['TL_LANG']['tl_dummyimages']['edit']   = array('', '');
+$GLOBALS['TL_LANG']['tl_dummyimages']['copy']   = array('', '');
+$GLOBALS['TL_LANG']['tl_dummyimages']['delete'] = array('', '');
+$GLOBALS['TL_LANG']['tl_dummyimages']['show']   = array('', '');
+
+?>
